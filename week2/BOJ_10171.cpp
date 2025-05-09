1b349c3132eec216b1174ba4c406c91cec21b053
holyocto <holyocto@naver.com>
2025-05-10 06:28:48 +0900

    week2

---
+++ week2/BOJ_10171.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cout << "\\    /\\\n"; //오옹 역슬래시는 두 번 써야 함함
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+    return 0;
+}
