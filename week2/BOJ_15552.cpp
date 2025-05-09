656956c4eceadab0aa346bb2fda6f6731eaba9fe
holyocto <holyocto@naver.com>
2025-05-10 08:10:13 +0900

    week2

---
+++ week2/BOJ_15552.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int T, a, b;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
