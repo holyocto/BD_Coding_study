18b414176e8ed782748a8424526370ce4597a4d2
holyocto <holyocto@naver.com>
2025-05-10 21:56:33 +0900

    week2

--- week3/BOJ_11328.cpp
+++ week3/BOJ_11328.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(void){
-    
+    int N;
+    cin >> N;
+    while(N--){
+        string s, t;
+        cin >> s >> t;
+        if (s.length() != t.length()){
+            cout << "Impossible\n";
+            continue;
+        }
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
 
+    if (s==t)
+        cout << "Possible\n";
+    else cout << "Impossible\n";
+    }
+    return 0;
 }
