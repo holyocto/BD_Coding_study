57da4cedea572caac0fee2aacab11228a87202e5
holyocto <holyocto@naver.com>
2025-05-26 22:19:48 +0900

    week4

--- week4/BOJ_5397.cpp
+++ week4/BOJ_5397.cpp
@@ -1,13 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+string K;
 
+int pre[5001];
+int nxt[5001];
+vector<int> v;
 
 int main(){
-    int N, K;
-    cin >> N >> K;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    
+    for (int i = 0; i<N; i++){
+        list<char> L = {};
+        auto p = L.begin();
 
-
-
-
+        cin >> K;
+        for (auto c : K){
+            if(c=='<'){
+                if (p!=L.begin()) p--;
+            }
+            else if (c=='>'){
+                if (p!=L.end())p++;
+            }
+            else if (c=='-'){
+                if (p!=L.begin()){
+                    p--;
+                    p=L.erase(p);
+                }
+            }
+            else L.insert(p,c);
+        }
+        for (auto c : L) cout << c;
+        cout << '\n';
+    }
 }
