ee8e9743dbd2a7597a1f9a000a1660a0ef5af300
holyocto <holyocto@naver.com>
2025-05-26 22:56:24 +0900

    week4

--- week4/BOJ_1158.cpp
+++ week4/BOJ_1158.cpp
@@ -1,11 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int caseN;
-    int L;
-    cin >> caseN;
+int main() {
+    int N, K;
+    cin >> N >> K;
 
-    
+    list<int> L;
+    for (int i = 1; i <= N; ++i)
+        L.push_back(i);
 
+    vector<int> result;
+    auto it = L.begin();
+
+    while (!L.empty()) {
+        for (int i = 1; i < K; ++i) {
+            ++it;
+            if (it == L.end()) it = L.begin();
+        }
+
+        result.push_back(*it);        
+        it = L.erase(it);               
+        if (it == L.end()) it = L.begin(); 
+    }
+
+    cout << "<";
+    for (size_t i = 0; i < result.size(); ++i) {
+        cout << result[i];
+        if (i != result.size() - 1) cout << ", ";
+    }
+    cout << ">\n";
+
+    return 0;
 }
